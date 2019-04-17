eabe5b1cd49a304cc44faae85c9daed47f3f8190
Hiroshi Iwata <iwaiwa2mail@gmail.com>
2019-04-17 16:45:17 +0900

    まとめると動かなかった

--- fs_csv_upload.py
+++ fs_csv_upload.py
@@ -6,7 +6,7 @@ import codecs
 import math
 import glob
 
-url_prefix = 'http://strawberry.iida.lab/syokkaen1/'
+url_prefix = 'http://strawberry.iida.lab/syokkaen2/'
 
 def temp2svp( temp ):
   temp = temp+273.15
@@ -29,56 +29,60 @@ if( len(sys.argv) < 2 ):
   sys.exit()
 '''
 
-data = []
+
+
+fiap = pyfiap.fiap.APP("http://iot.info.nara-k.ac.jp/axis2/services/FIAPStorage?wsdl")
 file_list = sorted(glob.glob('./data/*.csv'))
+no_file = 0
 for filename in file_list:
-  print(filename)
+#  print(filename)
+  data = []
   with codecs.open( filename, 'r', 'Shift-JIS' ) as f:
     for line in f:
       csv = line.strip().replace(" ", "").split(',')
       if(csv[0].isnumeric()):
         data.append(csv)
 
-upload = []
-prev = datetime.datetime(1900,1,1)
-fiap = pyfiap.fiap.APP("http://iot.info.nara-k.ac.jp/axis2/services/FIAPStorage?wsdl")
-for c in data:
-  y = int(c[0][0:4])
-  m = int(c[0][4:6])
-  d = int(c[0][6:8])
-  hh = int(c[1][0:2])
-  mm = int(c[1][2:4])
-  now = datetime.datetime(y,m,d,hh,mm)
-  if(now>prev):
-    bat = c[2] # バッテリー
-    lat = c[3] # 緯度
-    lon = c[4] # 経度
-    alt = c[5] # 高度
-    temp =c[6] # 温度
-    humi =c[7] # 湿度
-    pre = c[8] # 気圧
-    co2 = c[9] # CO2濃度
-    soil =c[10] # 土壌水分センサの生データ
-    vpd = round(calc_vpd(float(temp),float(humi)),2)
-    if( float(temp)>0 and float(humi)>0 and float(co2)>0 and float(pre)>0 and float(soil)>0 ):
-      upload.append([url_prefix+'temp', temp, now])
-      upload.append([url_prefix+'humi', humi, now])
-      upload.append([url_prefix+'vpd',  vpd,  now])
-      upload.append([url_prefix+'co2',  co2,  now])
-      upload.append([url_prefix+'pre',  pre,  now])
-      upload.append([url_prefix+'soil', soil, now])
-  prev = now
+  upload = []
+  prev = datetime.datetime(1900,1,1)
+  for c in data:
+    y = int(c[0][0:4])
+    m = int(c[0][4:6])
+    d = int(c[0][6:8])
+    hh = int(c[1][0:2])
+    mm = int(c[1][2:4])
+    now = datetime.datetime(y,m,d,hh,mm)
+    if(now>prev):
+      bat = c[2] # バッテリー
+      lat = c[3] # 緯度
+      lon = c[4] # 経度
+      alt = c[5] # 高度
+      temp =c[6] # 温度
+      humi =c[7] # 湿度
+      pre = c[8] # 気圧
+      co2 = c[9] # CO2濃度
+      soil =c[10] # 土壌水分センサの生データ
+      vpd = round(calc_vpd(float(temp),float(humi)),2)
+      if( float(temp)>0 and float(humi)>0 and float(co2)>0 and float(pre)>0 and float(soil)>0 ):
+        fiap.write([[url_prefix+'temp', temp, now],
+                    [url_prefix+'humi', humi, now],
+                    [url_prefix+'vpd', vpd, now],
+                    [url_prefix+'co2', co2, now],
+                    [url_prefix+'pre', pre, now],
+                    [url_prefix+'soil', soil, now],
+                    ])
+    prev = now
+  print(filename+" is uploaded. "+str(no_file)+" / "+len(file_list))
 '''
-      fiap.write([[url_prefix+'temp', temp, now],
-                  [url_prefix+'humi', humi, now],
-                  [url_prefix+'vpd', vpd, now],
-                  [url_prefix+'co2', co2, now],
-                  [url_prefix+'pre', pre, now],
-                  [url_prefix+'soil', soil, now],
-                  ])
+        upload.append([url_prefix+'temp', temp, now])
+        upload.append([url_prefix+'humi', humi, now])
+        upload.append([url_prefix+'vpd',  vpd,  now])
+        upload.append([url_prefix+'co2',  co2,  now])
+        upload.append([url_prefix+'pre',  pre,  now])
+        upload.append([url_prefix+'soil', soil, now])
+  fiap.write(upload)
 '''
-#fiap.write(upload)
-print(upload)
+#print(upload)
 #today = datetime.datetime.now()
 #today = datetime.datetime(2019,4,12,16,0,4)
 
